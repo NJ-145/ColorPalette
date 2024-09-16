82c8bddc1adcb881ab7aa350c641e686792f4f5f
“Nj” <s1110832058@gmail.com>
2024-09-16 17:18:35 +0800

    更新檔案架構

--- ColorPalette/Controller/MainViewController.swift
+++ ColorPalette/Controller/MainViewController.swift
@@ -24,15 +24,17 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     
     @IBOutlet var txvBlue: UITextField!
     
-    
-    // MARK: - Property
-    
     // MARK: - LifeCycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //vPalette.backgroundColor = UIColor.gray
+        UISettings()
         
+    }
+    
+    // MARK: - UI Settings
+    
+    func UISettings() {
         txvRed.delegate = self
         txvBlue.delegate = self
         txvGreen.delegate = self
@@ -42,72 +44,22 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         sldRed.maximumValue = 255
         sldRed.isContinuous = true
         sldRed.value = 0
-//        sldRed.addTarget(self, action: #selector(sldRedValueChanged(_:)), for: .valueChanged)
-//        sldRed.addTarget(self, action:
-//            #selector(sliderSum(_:)), for: .valueChanged)
-        /*sldRed.addTarget(
-         Any?, action: Selector,
-         for: UIControl.Event)*/
         
         //設定滑桿最大最小值
         sldGreen.minimumValue = 0
         sldGreen.maximumValue = 255
         sldGreen.isContinuous = true
         sldGreen.value = 0
-//        sldGreen.addTarget(self, action:
-//            #selector(sldGreenValueChanged(_:)), for: .valueChanged)
-//        sldGreen.addTarget(self, action:
-//            #selector(sliderSum(_:)), for: .valueChanged)
+
         
         //設定滑桿最大最小值
         sldBlue.minimumValue = 0
         sldBlue.maximumValue = 255
         sldBlue.isContinuous = true
         sldBlue.value = 0
-//        sldBlue.addTarget(self, action: #selector(sldBlueValueChanged(_:)), for: .valueChanged)
-//        sldBlue.addTarget(self, action:
-//            #selector(sliderSum(_:)), for: .valueChanged)
-        
     }
     
-//    @IBAction func sldGreenValueChanged(_ sender: UISlider) {
-//        if sender == sldGreen {
-//            txvGreen.text = String(Int(sender.value))
-//        }
-//    }
-//    
-//    @IBAction func sldBlueValueChanged(_ sender: UISlider) {
-//        if sender == sldBlue {
-//            txvBlue.text = String(Int(sender.value))
-//        }
-//    }
-//    
-//    @IBAction func sldRedValueChanged(_ sender: UISlider) {
-//        if sender == sldRed {
-//            txvRed.text = String(Int(sender.value))
-//        }
-//    }
-    
-    //限制輸入匡只能輸入數字
-    func textField(_ textField:UITextField,shouldChangeCharactersIn range:NSRange,replacementString string: String) -> Bool {
-        
-        let allowedCharacters = CharacterSet.decimalDigits
-        let characterSet = CharacterSet(charactersIn: string)
-        if !allowedCharacters.isSuperset(of: characterSet){
-            return false
-        }
-        
-        let currentText = (textField.text ?? "") as NSString
-        let newString = currentText.replacingCharacters(in: range, with: string)
-        
-        if let number = Int(newString), number >= 0{
-            return true
-        } else if newString.isEmpty{
-            return true
-        } else{
-            return false
-        }
-    }
+    // MARK: - IBAction
     
     //控制紅色的TextField輸入的數值如果超過255,直接變成255
     @IBAction func limitationsRad(_ sender: Any) {
@@ -147,11 +99,6 @@ class MainViewController: UIViewController, UITextFieldDelegate {
             txvBlue.text = String(Int(sender.value))
         }
     }
-    /*@IBAction func sliderToText(_ sender: UISlider){
-        txvRed.text = "\(Int(sldRed.value))"
-        txvGreen.text = "\(Int(sldGreen.value))"
-        txvBlue.text = "\(Int(sldBlue.value))"
-        }*/
     
     //調色盤顏色跟著滑桿數值變動
     @IBAction func sliderSum(_ sender: Any){
@@ -189,31 +136,26 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    /*@IBAction func textToSlider(_ sender: UITextField){
-        if let redStringValue = txvRed.text{
-            if let redIntValue = Int(redStringValue){
-                sldRed.setValue(Float(redIntValue), animated: true)
-            }
-        }
-        if let greenStringValue = txvGreen.text{
-            if let greenIntValue = Int(greenStringValue){
-                sldGreen.setValue(Float(greenIntValue), animated: true)
-            }
-        }
-        if let blueStringValue = txvBlue.text{
-            if let blueIntValue = Int(blueStringValue){
-                sldBlue.setValue(Float(blueIntValue), animated: true)
-            }
-        }
-    }*/
+    // MARK: - Function
     
-        // MARK: - UI Settings
+    //限制輸入匡只能輸入數字
+    func textField(_ textField:UITextField,shouldChangeCharactersIn range:NSRange,replacementString string: String) -> Bool {
         
-        // MARK: - IBAction
+        let allowedCharacters = CharacterSet.decimalDigits
+        let characterSet = CharacterSet(charactersIn: string)
+        if !allowedCharacters.isSuperset(of: characterSet){
+            return false
+        }
         
-        // MARK: - Function
+        let currentText = (textField.text ?? "") as NSString
+        let newString = currentText.replacingCharacters(in: range, with: string)
         
-    
-    // MARK: - Extensions
-    
+        if let number = Int(newString), number >= 0{
+            return true
+        } else if newString.isEmpty{
+            return true
+        } else{
+            return false
+        }
+    }
 }
